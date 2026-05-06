756d4985e407bdfbfda3c02f94164598d7f8f60f
Alisa <alisa.faloba@stud.ubbcluj.ro>
2026-05-06 18:59:27 +0300

    added getters and setters for user

--- src/main/java/com/alisafaloba/trainbooking/Domain/User.java
+++ src/main/java/com/alisafaloba/trainbooking/Domain/User.java
@@ -22,4 +22,28 @@ public class User {
         this.email = email;
         this.role = role;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
